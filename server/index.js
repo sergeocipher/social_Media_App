e13b127b00b343d9536720e6347593f08bdf9a66
Somyajit <somyajitsaha621@gmail.com>
2025-09-01 11:16:03 +0530

    initial setup

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+// Logging middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.listen(8000, () => {
+    console.log(`Server running on port 8000`);
+});
